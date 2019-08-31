c606e5f10b6db2821b74d3953cd9fcfdb2cc65f5
fponce06 <fponce@espol.edu.ec>
2019-08-31 12:43:02 -0500

    verificacion

--- src/Juego/Inicio.java
+++ src/Juego/Inicio.java
@@ -5,10 +5,76 @@
  */
 package Juego;
 
+import armatupalabrap.ArmaTuPalabraP;
+import java.util.ArrayList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Freddy
  */
 public class Inicio {
+
+    private HBox root = new HBox(20);
+    private ImageView imagen = new ImageView(new Image("/imagenes/abeja.jpg"));
+    private VBox inicio = new VBox(20);
+    private HBox datos = new HBox(20);
+    private Label usuario = new Label("Usuario");
+    private TextField txtusuario = new TextField();
+    private HBox datos2 = new HBox(20);
+    private Label contrasena = new Label("Contraseña");
+    private TextField txtcontrasena = new TextField();
+    private Button registro = new Button("Registrar");
+    private Button empieza = new Button("Inicio");
+    public static Registro rg = new Registro();
+    public static Stage reg = new Stage();
+    public static boolean empez = false;
+    public static Jugador player;
+    private ComboBox<String> idiomas = new ComboBox();
+
+    /**
+     * constructor para ordenar nuestros elementos de la interfaz y definir las
+     * acciones de los botones
+     */
+    public Inicio() {
+        organizarElementos();
+        idiomas.setOnAction(e -> InterfazUsu.cargarDatosIdioma("italiano"));
+        empieza.setOnAction(e -> empezar());
+        registro.setOnAction(e -> registrar());
+    }
     
+    /**
+     * metodo que organiza los elementos de nuestra interfaz
+     */
+    public void organizarElementos() {
+        llenarComboBox();
+        imagen.setFitHeight(450);
+        imagen.setFitWidth(350);
+        datos.getChildren().addAll(usuario, txtusuario);
+        datos.setAlignment(Pos.CENTER);
+        datos2.getChildren().addAll(contrasena,txtcontrasena);
+        datos2.setAlignment(Pos.CENTER);
+        inicio.getChildren().addAll(datos, datos2,new Label("Idiomas"),idiomas, registro, empieza);
+        inicio.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(imagen, inicio);
+        root.setAlignment(Pos.CENTER);
+    }
+    
+    /**
+     * 
+     * @return getter que retorna el rut que se presentara por pantalla 
+     */
+    public HBox getRoot() {
+        return root;
+    }
 }
